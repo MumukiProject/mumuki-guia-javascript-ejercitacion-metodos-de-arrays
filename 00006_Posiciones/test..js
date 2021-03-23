9928f54f0806cd95be6b8bd21c526c73494679ed
mayra@mumuki.org <mayra@mumuki.org>
2021-03-23 17:30:35 +0000

    Mumuki Export on 2021-03-23 17:30:35 +0000

--- 00006_Posiciones/test..js
+++ 00006_Posiciones/test..js
@@ -1,5 +1,4 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it(`Si hacemos posiciones(['Everlong', 'The Pretender', 'Learn to Fly']) debería retornar [ '0 - Everlong', '1 - The Pretender', '2 - Learn to Fly' ]`, function() {
+  let resultado = posiciones(['Everlong', 'The Pretender', 'Learn to Fly'])
+  assert.equal(resultado, ['0 - Everlong', '1 - The Pretender', '2 - Learn to Fly'],`posiciones debería retornar ['0 - Everlong', '1 - The Pretender', '2 - Learn to Fly'] pero fue ${resultado}`);
 })
