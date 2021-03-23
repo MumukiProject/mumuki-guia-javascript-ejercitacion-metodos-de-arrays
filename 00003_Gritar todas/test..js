9d929efac3f9258229ddd626c688019e6a74ac37
mayra@mumuki.org <mayra@mumuki.org>
2021-03-23 16:03:43 +0000

    Mumuki Export on 2021-03-23 16:03:43 +0000

--- 00003_Gritar todas/test..js
+++ 00003_Gritar todas/test..js
@@ -1,5 +1,4 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it(`Si hacemos gritarTodas(['Labore sea dolor.', 'Justo rebum dolor.', 'Stet lorem amet.']) debería retornar [6, 14, 26, 198]`, function() {
+  let resultado = gritarTodas(['Labore sea dolor.', 'Justo rebum dolor.', 'Stet lorem amet.'])
+  assert.equal(resultado, ['¡Labore sea dolor.!', '¡Justo rebum dolor.!', '¡Stet lorem amet.!'],`gritarTodas debería retornar ['¡Labore sea dolor.!', '¡Justo rebum dolor.!', '¡Stet lorem amet.!'] pero fue ${resultado}`);
 })
