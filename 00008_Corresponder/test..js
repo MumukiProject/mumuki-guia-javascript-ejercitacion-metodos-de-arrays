06cfd72fac13a37441f7c8e59168502ac61ee636
mayra@mumuki.org <mayra@mumuki.org>
2021-03-23 18:36:06 +0000

    Mumuki Export on 2021-03-23 18:36:06 +0000

--- 00008_Corresponder/test..js
+++ 00008_Corresponder/test..js
@@ -1,5 +1,4 @@
-describe("Group description", function() {
-  it("Sample description", function() {
-    assert(true)
-  })
+it(`Si hacemos corresponder([12.5, 56, 98],['celular', 'notebook', 'monitor']) debería retornar ['celular 22.6875', 'notebook 101.64', 'monitor 177.87']`, function() {
+  let resultado = corresponder([12.5, 56, 98],['celular', 'notebook', 'monitor'])
+  assert.equal(resultado, ['celular 22.6875', 'notebook 101.64', 'monitor 177.87'],`corresponder debería retornar ['celular 22.6875', 'notebook 101.64', 'monitor 177.87'] pero fue ${resultado}`);
 })
