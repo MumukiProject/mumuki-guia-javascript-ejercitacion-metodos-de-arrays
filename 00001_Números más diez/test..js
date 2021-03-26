359b85382e5d085ede4c72283d60908a8df6b612
mayra@mumuki.org <mayra@mumuki.org>
2021-03-26 19:44:57 +0000

    Mumuki Export on 2021-03-26 19:44:57 +0000

--- 00001_Números más diez/test..js
+++ 00001_Números más diez/test..js
@@ -1,9 +1,9 @@
 it(`Si hacemos numerosMasDiez([1, 2, 3, 4, 5]) debería retornar [10, 20, 30, 40, 50]`, function() {
   let resultado = numerosMasDiez([1, 2, 3, 4, 5])
-  assert.equal(resultado, [10, 20, 30, 40, 50],`numerosMasDiez debería retornar [10, 20, 30, 40, 50] pero fue ${resultado}`);
+  assert.deepEqual(resultado, [10, 20, 30, 40, 50],`numerosMasDiez debería retornar [10, 20, 30, 40, 50] pero fue ${resultado}`);
 })
 
 it(`Si hacemos numerosMasDiez([15, 22, 33, 40, 65]) debería retornar [25, 32, 43, 50, 75]`, function() {
   let resultado = numerosMasDiez([15, 22, 33, 40, 65])
-  assert.equal(resultado, [25, 32, 43, 50, 75],`numerosMasDiez debería retornar [25, 32, 43, 50, 75] pero fue ${resultado}`);
+  assert.deepEqual(resultado, [25, 32, 43, 50, 75],`numerosMasDiez debería retornar [25, 32, 43, 50, 75] pero fue ${resultado}`);
 })
